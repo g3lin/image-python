7b137b8546c19f5519ad73a051493db2f8407c71
Antoine Gélin <gelinantoine@gmail.com>
2018-02-16 14:35:23 +0100

    add seam detection v2

--- main.py
+++ main.py
@@ -12,27 +12,27 @@ def calculate_cost_matrix(path_image_grad):
         tabline = []
 
         for x in range(0, im.size[0]):
-            cost = pix[x,y]
+            cost = pix[x,y][0]
             if y>0:
                 if x-1 < 0:
                     # cas où on est à gauche de l'image
-                    c2 = pix[x,y-1]
-                    c3 = pix[x+1,y-1]
-                    cost = pix[x,y] + min(c2,c3)
+                    c2 = pix[x,y-1][0]
+                    c3 = pix[x+1,y-1][0]
+                    cost = pix[x,y][0] + min(c2,c3)
 
 
                 elif x+1 >= im.size[0]:
                     # cas où on est vers le milieu
-                    c1 = pix[x-1,y-1]
-                    c2 = pix[x,y-1]
-                    cost = pix[x,y] + min(c1,c2)
+                    c1 = pix[x-1,y-1][0]
+                    c2 = pix[x,y-1][0]
+                    cost = pix[x,y][0] + min(c1,c2)
 
                 else:
                     # cas où on est vers la droite
-                    c1 = pix[x-1,y-1]
-                    c2 = pix[x,y-1]
-                    c3 = pix[x+1,y-1]
-                    cost = pix[x,y] + min(c1,c2,c3)
+                    c1 = pix[x-1,y-1][0]
+                    c2 = pix[x,y-1][0]
+                    c3 = pix[x+1,y-1][0]
+                    cost = pix[x,y][0] + min(c1,c2,c3)
             tabline += [cost]
 
 
@@ -40,14 +40,37 @@ def calculate_cost_matrix(path_image_grad):
     
     return cost_matrix
 
+def detect_seam(cost_matrix):
+    seam = []
+    y = len(cost_matrix)-1
+    min_x_value = cost_matrix[y][0]
+    min_x = 0
+    for x in range( len(cost_matrix[1])-1 ):
+        if cost_matrix[y][x] < min_x_value:
+            min_x_value = cost_matrix[y][x]
+            min_x = x
+    x = min_x
+    seam += [(x,y)]
 
 
+    for y in range(len(cost_matrix)-2, -1,-1):
+        min_x_value = cost_matrix[y][x]
+        min_x = x
+        for x in range( x-1,x+2 ):
+            if cost_matrix[y][x] < min_x_value :
+                min_x_value = cost_matrix[y][x]
+                min_x = x
+        x = min_x
+        seam += [(x,y)]
 
+    return seam
 
 
 def main():
     # test data
-    calculate_cost_matrix('/home/antoine/dev/data_images/1g.jpg')
+    cm = calculate_cost_matrix('/home/antoine/dev/data_images/1g.jpg')
+    sm = detect_seam(cm)
+
 
 if __name__ == "__main__":
     main()
