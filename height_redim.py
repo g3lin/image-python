b402774940de4f65a42a2e0ea4fe32327538d050
lapertor <lapertor@pc5401f.lan.esiee.fr>
2018-02-16 16:31:49 +0100

    Application du seam carving horizontal à partir du seam carving vertical

--- height_redim.py
+++ height_redim.py
@@ -1,11 +1,15 @@
 '''
-A ce stade, on a : 
+A ce stade, on dispose de : 
     - une image à traiter
-    - sa carte d'énergie (Anthony)
-    - une fonction (celle d'Antoine) detect_seam utilisant la carte d'énergie, et qui retourne la seam
-        à enlever sous forme de liste de coordonnées
-        - cette fonction s'aide d'une fonction calculate_cost qui calcule le coût d'un pixel dans l'image  
+    - (Anthony) sa carte d'énergie
+    - (Antoine) une fonction calculate_cost_matrix qui détermine une matrice des coûts de l'image
+    - (Antoine) une fonction detect_seam utilisant la matrice de coûts, et qui retourne la seam verticale
+        à enlever sous forme de tuple de coordonnées
+    - (Elie) une fonction qui enlève le seam demandé sur l'image
+    - (Elie) une fonction qui décale l'image pour compenser la suppression du seam
 
+Tout cela applique un seam carving vertical
+Ce fichier permet d'appliquer un seam carving horizontal à partir du seam carving vertical déjà codé> 
 '''
 
 from PIL import Image
@@ -13,13 +17,23 @@ from seam import detect_seam calculate_cost_matrix
 from elie import remove_seam shift_left
 
 def height_redim(im, height, im_grad):
+    '''
+    Cette fonction : 
+        - tourne l'image et le gradient de l'image à 90° à droite
+        - applique le seam carving sur les images tournées jusqu'à ce que la hauteur voulue soit atteinte
+        - re-tourne l'image et la renvoie
+    '''
     rotated = im.rotate(270)
     rotated_grad = im_grad.rotate(270)
     
     # Utilisation de l'algorithme d'Antoine
-    while rotated.size[0] > height: # >= peut etre ?
+    while rotated.size[0] > height:                                     # >= peut etre ?
+
+        # Partie d'Antoine (seam.py)
         cost_matrix = calculate_cost_matrix(rotated_grad)
         seam = detect_seam(cost_matrix)
+
+        # Partie d'Elie (elie.py)
         rotated = remove_seam(rotated, seam)
         rotated = shift_left(rotated, 1)
     
@@ -28,6 +42,9 @@ def height_redim(im, height, im_grad):
 
 
 def main():
+    '''
+    Fonction principale
+    '''
     im = Image.open("../1.jpg")
     hr = height_redim(im, 300)
     return hr
