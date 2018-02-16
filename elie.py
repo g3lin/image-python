d042878cf0c3497d1e9dada6019c2a3784f9ddcb
Elie Haidamous <elie.haidamous@edu.esiee.fr>
2018-02-16 15:15:50 +0100

    deux fonctions remove et shift

--- elie.py
+++ elie.py
@@ -1,7 +1,18 @@
 from PIL import Image
 
 def main():
-    
+
+def remove_seam(im,seam):
+    for x in range (im.size[0]):
+        for elm in seam:
+            if x > elm[0]:
+                shift_left(im,1)
+    return im
+
+def shift_left(im,nombre):
+    image = im.load()
+    for x in range(im.size[0]):
+        image[x,y]= image[x-nombre,y]
 
 if __main__ == "__main__":
     main()
