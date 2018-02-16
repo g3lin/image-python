c2c9daa3983d9a730b2320a4004803253307c0a6
lapertor <lapertor@pc5401f.lan.esiee.fr>
2018-02-16 16:13:00 +0100

    Application horizontale du seam carving

--- height_redim.py
+++ height_redim.py
@@ -10,13 +10,18 @@ A ce stade, on a :
 
 from PIL import Image
 from seam import detect_seam calculate_cost_matrix 
+from elie import remove_seam shift_left
 
 def height_redim(im, height, im_grad):
     rotated = im.rotate(270)
+    rotated_grad = im_grad.rotate(270)
     
     # Utilisation de l'algorithme d'Antoine
     while rotated.size[0] > height: # >= peut etre ?
-        seam_carving(rotated)
+        cost_matrix = calculate_cost_matrix(rotated_grad)
+        seam = detect_seam(cost_matrix)
+        rotated = remove_seam(rotated, seam)
+        rotated = shift_left(rotated, 1)
     
     return rotated.rotate(90)
 
