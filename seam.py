0a7369ecc671dd86b406f1ff624e8d7294aa15d4
Antoine Gélin <gelinantoine@gmail.com>
2018-03-30 16:51:44 +0200

    replace python arrays by numpy arrays in calculate_cost_matrix().  Not sure if effective ¯\_(ツ)_/¯

--- seam.py
+++ seam.py
@@ -14,6 +14,7 @@ les x et y du pixel à supprimer
 '''
 
 from PIL import Image
+import numpy as np
 
 def calculate_cost_matrix(image_grad):
     '''
@@ -21,47 +22,47 @@ def calculate_cost_matrix(image_grad):
     gradient, et la retourne
     '''
     im = image_grad
-    cost_matrix = []    
+    cost_matrix = np.empty( dtype=np.int8, shape=(im.size[1],im.size[0]) )  
     pix = im.load() # on cahrge l'image
 
     # Calcul de la matrice des couts
     for y in range(0,im.size[1]):
-        tabline = [] #on crée un nouveau tableau pour chaque ligne du tableau
+        #on crée un nouveau tableau pour chaque ligne du tableau
 
         for x in range(0, im.size[0]):
             
             if y>0:
+                lastline = cost_matrix[y-1] 
                 if x-1 < 0:
                     # cas où on est à gauche de l'image
-                    c2 = cost_matrix[len(cost_matrix)-1][x] 
-                    c3 = cost_matrix[len(cost_matrix)-1][x+1]
+                    c2 = lastline[x] 
+                    c3 = lastline[x+1]
                     # le cout du pixel est la somme la valeur du pixel avec le minimum du cout du pixel en haut et celui en haut à droite
                     cost = pix[x,y] + min(c2,c3)
 
 
                 elif x+1 >= im.size[0]:
                     # cas où on est vers le milieu
-                    c1 = cost_matrix[len(cost_matrix)-1][x-1]
-                    c2 = cost_matrix[len(cost_matrix)-1][x]
+                    c1 = lastline[x-1]
+                    c2 = lastline[x]
                     # le cout du pixel est la somme la valeur du pixel avec le minimum du cout du pixel en haut et celui en haut à gauche
                     cost = pix[x,y] + min(c1,c2)
 
                 else:
                     # cas où on est vers la droite
-                    c1 = cost_matrix[len(cost_matrix)-1][x-1]
-                    c2 = cost_matrix[len(cost_matrix)-1][x]
-                    c3 = cost_matrix[len(cost_matrix)-1][x+1]
+                    c1 = lastline[x-1]
+                    c2 = lastline[x]
+                    c3 = lastline[x+1]
                     # le cout du pixel est la somme la valeur du pixel avec le minimum du cout du pixel en haut et celui en haut à droite et celui en haut à gauche
                     cost = pix[x,y] + min(c1,c2,c3)
             else:
                 cost = pix[x,y] 
                 # si on est dans la ligne du haut ( y = 0 ), on va juste mettre en cout la valeur du pixel
             # on rajoute à la ligne le cout du pixel
-            tabline += [cost]
+            cost_matrix[y][x] = cost[0]
 
         # on rajoute à la matrice de couts la lgne deds couts
-        cost_matrix += [tabline]    
-    
+        
     return cost_matrix
 
 def detect_seam(cost_matrix):
