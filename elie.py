15308f106526b09220a318e1f48b42e8a780e1a3
haidamoe <haidamoe@pc5401c.lan.esiee.fr>
2018-03-30 16:59:45 +0200

    test sm

--- elie.py
+++ elie.py
@@ -27,23 +27,12 @@ def remove_seam(im,seam):
     image = im.load()
     for elm in seam: #pour chaque element du seam (de la forme (x,y))
         for x in range (elm[0],im.size[0]-1): #pour chaque x 
-                image[x,elm[1]] = image[x+1,elm[1]]
-    final =  resize(im,(im.size[0]-1,im.size[1]))    
-    return final #l'image modifiée est retournée
-
-def resize(im, dec):
-    '''
-    Cette fonction retourne l'image donnée redimensionnée
-    aux dimensions souhaitées
-    '''
-    image2 = Image.new("RGB",(dec[0],dec[1]))
-    image2_ = image2.load()
-    image = im.load()
-    for y in range (image2.size[1]):
-        for x in range (image2.size[0]):
-            image2_[x,y]=image[x*im.size[0]//image2.size[0],y*im.size[1]//image2.size[1]]
-    return image2     
-
+            image[x,elm[1]] = image[x+1,elm[1]]
+    #final =  resize(im,(im.size[0]-1,im.size[1]))
+    box = (0,0,im.size[0]-1,im.size[1])
+    im2 = im.crop(box)    
+    return  im2 #l'image modifiée est retournée
+    
 def main():
     '''
     Fonction principale : charge une image et son gradient et applique
@@ -54,18 +43,19 @@ def main():
     en affichant l'image à chaque fin de boucle pour suivre l'évolution
     '''
     im = Image.open('1.jpg')
-    image_smc = Image.new("RGB",(im.size[0],im.size[1]))
+    #image = im.load()
     img = dual_gradient.gradient(im)
-    image_smc = im
-    # img = Image.open('1g.jpg')
-    compteur = 10
+    compteur = 11
     while compteur!=0:
         cm = seam.calculate_cost_matrix(img)
         sm = seam.detect_seam(cm)
-        image_smc = remove_seam(image_smc,sm)
-        #img = remove_seam(img,sm)
+        for elm in sm: #pour chaque element du seam (de la forme (x,y))
+            im.load()[elm[0],elm[1]]= 255, 0, 0
+        #im.show()
+        im = remove_seam(im,sm)
+        img = remove_seam(img,sm)
         compteur-=1
-    image_smc.show()
+    im.show()
     #img.show()
 if __name__ == "__main__":
     main()
