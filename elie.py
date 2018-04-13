aa3c4a1f70b2f13681fd407bb5d62cf2e7649d1a
haidamoe <haidamoe@pc5301j.lan.esiee.fr>
2018-04-13 13:59:35 +0200

    fichier main pour les test

--- elie.py
+++ elie.py
@@ -32,30 +32,3 @@ def remove_seam(im,seam):
     box = (0,0,im.size[0]-1,im.size[1])
     im2 = im.crop(box)    
     return  im2 #l'image modifiée est retournée
-    
-def main():
-    '''
-    Fonction principale : charge une image et son gradient et applique
-    un seam carving k fois en plusieurs étapes ... : 
-        - Calcul d'une matrice de coût de l'image à partir de son gradient
-        - Detection de la seam optimale à partir de la pmatrice de coût
-        - Suppression de la seam déterminée
-    en affichant l'image à chaque fin de boucle pour suivre l'évolution
-    '''
-    im = Image.open('1.jpg')
-    #image = im.load()
-    img = dual_gradient.gradient(im)
-    compteur = 11
-    while compteur!=0:
-        cm = seam.calculate_cost_matrix(img)
-        sm = seam.detect_seam(cm)
-        for elm in sm: #pour chaque element du seam (de la forme (x,y))
-            im.load()[elm[0],elm[1]]= 255, 0, 0
-        #im.show()
-        im = remove_seam(im,sm)
-        img = remove_seam(img,sm)
-        compteur-=1
-    im.show()
-    #img.show()
-if __name__ == "__main__":
-    main()
